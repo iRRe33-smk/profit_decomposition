82d9eae5cbf1d515fce7e3b114b6e5ccfe00b24a
IsakBerntsson <isak.berntsson@dspanel.com>
2022-10-07 11:50:16 +0200

    datastructsPAM
    Started work on mapping out datastructures and dataflow

---
+++ PAM/main.m
@@ -0,0 +1,38 @@
+% N = [1,3,4,3]
+% h_i,t=[0,15000000,1,10,0,0,0,1]
+% C_B = [123,0.0025,145123,]
+%
+%
+%
+
+
+
+
+for t = 1:T
+    %datauppdatering
+    %term1
+    %term2
+    %%...
+
+end
+    
+    
+    
+    
+
+
+
+
+
+
+% priset i T_0 = prislista * dics(ränta)
+% T_1 = P_t0 + deltaP(riskfakt)
+
+
+
+
+
+
+
+
+
