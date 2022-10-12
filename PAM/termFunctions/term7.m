501156b7057dcb18395787d669f87230ea6505ef
IsakBerntsson <isak.berntsson@dspanel.com>
2022-10-12 22:27:52 +0200

    last min changes

--- PAM/termFunctions/term7.m
+++ PAM/termFunctions/term7.m
@@ -1,12 +1,13 @@
-function [T7] = term7(h, P, D, f, df)
+function [T7] = term7(h, P, Dt, f, df)
 %TERM7 seventh term of PAM -> array[nP]
 %   TODO error-termen tas ej med här. Gör separat funktion för den.
 %   Antar att vi vill hålla den separat för att kunna bdöma felet.
 %   h = holding
-%   D = dividends
+%   Dt = dividends paid out in this timestep, matrix[nP, nC]
 %   f = FX-rates
 %   df = daily change in FX-rates
-%
+
+D_local
 
 T7 = h .* ( P - D * f ) * df;
 
