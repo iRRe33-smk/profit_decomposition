c91765da9d1f518476dfd1359f1104a0c1372751
einaaar <73593460+einaaar@users.noreply.github.com>
2022-12-21 14:44:01 +0100

    Revert "Revert "Uppdaterad term6 etc.""
    This reverts commit 684e64463043a8e1cefbd0a7ee53a686e81626d2.

--- PAM/PriceEquation/getForwardRate.m
+++ PAM/PriceEquation/getForwardRate.m
@@ -1,17 +1,26 @@
 function [forward_rates]=getForwardRate()
 if ispc
     cd("PriceEquation\")
-    myDir = ".\3MonthCurves\";
+    myDir = ".\120daysCurves\";
 elseif ismac
     cd("PriceEquation/")
-    myDir = "./3MonthCurves/";
+    myDir = "./120daysCurves/";
 end
 %myDir = "\\ad.liu.se\home\einei581\Documents\CDIO\profit_decomposition-main (1)\profit_decomposition-main\PAM\termFunctions\3MonthCurves\";
 %myDir = ".\3MonthCurves\";
 myFiles = dir(fullfile(myDir,'*.mat'));
 %disp(myFiles)
-for k = 1:length(myFiles)/2
-    baseFileName = myFiles(2*k-1).name;
+% for k = 1:length(myFiles)/2
+%     baseFileName = myFiles(2*k-1).name;
+%     currency_temp =erase(baseFileName, ".mat");
+%     fullFileName = fullfile(myDir,baseFileName);
+%     forward_rate_temp = load(fullFileName);
+%     forward_rates(k,:) = {currency_temp;forward_rate_temp.f'};
+% 
+% end
+
+for k = 1:length(myFiles)
+    baseFileName = myFiles(k).name;
     currency_temp =erase(baseFileName, ".mat");
     fullFileName = fullfile(myDir,baseFileName);
     forward_rate_temp = load(fullFileName);
